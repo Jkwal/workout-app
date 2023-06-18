3b0c569c188949bac69b4a6f9c2304b39a024b5d
Yauheny Vial <mr.jk1629003@gmail.com>
2023-06-18 16:36:02 +0300

    feat: add prisma

--- server/server.js
+++ server/server.js
@@ -1,25 +1,38 @@
 import 'colors'
+import * as dotenv from 'dotenv'
 import express from 'express'
+import morgan from 'morgan'
 
 import authRoutes from './app/auth/auth.routes.js'
+import { prisma } from './app/prisma.js'
+
+dotenv.config()
 
 const app = express()
 
 async function main() {
-	// if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
+	if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
 	app.use(express.json())
 	app.use('/api/auth', authRoutes)
 
-	const PORT = 5000
+	const PORT = process.env.PORT || 5000
 
 	app.listen(
 		PORT,
 		console.log(
-			`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green
+			`🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue
 				.bold
 		)
 	)
 }
 
 main()
+	.then(async () => {
+		await prisma.$disconnect()
+	})
+	.catch(async e => {
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})

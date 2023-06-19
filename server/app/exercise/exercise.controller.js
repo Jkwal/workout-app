9a78d20688f5e988543767875eaccde6d65281ee
Yauheny Vial <mr.jk1629003@gmail.com>
2023-06-19 22:56:09 +0300

    feat: add logic update/delete exercises

--- server/app/exercise/exercise.controller.js
+++ server/app/exercise/exercise.controller.js
@@ -19,11 +19,56 @@ export const createNewExercise = asyncHandler(async (req, res) => {
 	res.json(exercise)
 })
 
+// @desc   Update exercise
+// @route  PUT /api/exercises/:id
+// @access Private
+export const updateExercise = asyncHandler(async (req, res) => {
+	const { name, times, iconPath } = req.body
+
+	try {
+		const exercise = await prisma.exercise.update({
+			where: {
+				id: Number(req.params.id)
+			},
+			data: {
+				name,
+				times,
+				iconPath
+			}
+		})
+		res.json(exercise)
+	} catch (error) {
+		res.status(404)
+		throw new Error('Exercise not found')
+	}
+})
+
+// @desc   Delete exercise
+// @route  DELETE /api/exercises/:id
+// @access Private
+export const deleteExercise = asyncHandler(async (req, res) => {
+	try {
+		const exercise = await prisma.exercise.delete({
+			where: {
+				id: Number(req.params.id)
+			}
+		})
+		res.json({ message: 'Exercise deleted!' })
+	} catch (error) {
+		res.status(404)
+		throw new Error('Exercise not found')
+	}
+})
+
 // @desc   Get exercises
 // @route  Get /api/exercises
 // @access Private
 export const getExercises = asyncHandler(async (req, res) => {
-	const exercises = await prisma.exercise.findMany()
+	const exercises = await prisma.exercise.findMany({
+		orderBy: {
+			createdAt: 'desc'
+		}
+	})
 
 	res.json(exercises)
 })

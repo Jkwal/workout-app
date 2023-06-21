78cc8f64b745fd7274fdbd1fbecc99cd59879cdc
Yauheny Vial <mr.jk1629003@gmail.com>
2023-06-21 20:56:25 +0300

    feat: add createNewWorkoutLog

--- server/app/workout/workout.routes.js
+++ server/app/workout/workout.routes.js
@@ -2,6 +2,7 @@ import express from 'express'
 
 import { protect } from '../middleware/auth.middleware.js'
 
+import { createNewWorkoutLog } from './log/workout-log.controller.js'
 import {
 	createNewWorkout,
 	deleteWorkout,
@@ -19,4 +20,6 @@ router
 	.get(protect, getWorkout)
 	.put(protect, updateWorkout)
 	.delete(protect, deleteWorkout)
+
+router.route('/log/:id').post(protect, createNewWorkoutLog)
 export default router

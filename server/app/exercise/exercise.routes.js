ed05a4a1d5a1e0dc4822de65511f949435b0b7c3
Yauheny Vial <mr.jk1629003@gmail.com>
2023-06-21 18:49:38 +0300

    feat: add exercise log CRUD

--- server/app/exercise/exercise.routes.js
+++ server/app/exercise/exercise.routes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import {protect} from '../middleware/auth.middleware.js'
+import { protect } from '../middleware/auth.middleware.js'
 
 import {
 	createNewExercise,
@@ -8,7 +8,12 @@ import {
 	getExercises,
 	updateExercise
 } from './exercise.controller.js'
-import {createNewExerciseLog} from "./log/exercise-log.controller.js";
+import { createNewExerciseLog } from './log/exercise-log.controller.js'
+import { getExerciseLog } from './log/get-exercise-log.controller.js'
+import {
+	completeExerciseLog,
+	updateExerciseLogTime
+} from './log/update-exercise-log-controller.js'
 
 const router = express.Router()
 
@@ -18,5 +23,12 @@ router
 	.put(protect, updateExercise)
 	.delete(protect, deleteExercise)
 
-router.route('/log/:exerciseId').post(protect, createNewExerciseLog)
+router
+	.route('/log/:id')
+	.post(protect, createNewExerciseLog)
+	.get(protect, getExerciseLog)
+
+router.route('/log/time/:id').put(protect, updateExerciseLogTime)
+
+router.route('/log/complete/:id').patch(protect, completeExerciseLog)
 export default router

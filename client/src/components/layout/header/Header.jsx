f86f88f8dc86d383b1a7dd1094e3ec5d470dea01
Yauheny Vial <mr.jk1629003@gmail.com>
2023-06-25 13:16:20 +0300

    feat: add auth page

--- client/src/components/layout/header/Header.jsx
+++ client/src/components/layout/header/Header.jsx
@@ -27,7 +27,7 @@ export const Header = ({ backLink = '/' }) => {
 			) : (
 				<button
 					onClick={() => {
-						navigate('/profile')
+						navigate(isAuth ? '/profile' : '/auth')
 					}}
 				>
 					<SlUser fill="#fff" fontSize={27} />
--- client/src/components/screens/auth/Auth.jsx
+++ client/src/components/screens/auth/Auth.jsx
@@ -1,3 +1,64 @@
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+
+import { Loader } from '../../ui/Loader.jsx'
+import { Button } from '../../ui/button/Button.jsx'
+import { Field } from '../../ui/field/Field.jsx'
+
+import { Layout } from '../../layout/Layout.jsx'
+
+import styles from './Auth.module.scss'
+
 export const Auth = () => {
-	return <div>Auth</div>
+	const isLoading = false
+	const isLoadingAuth = false
+
+	const [type, setType] = useState('auth')
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm({
+		mode: 'onChange'
+	})
+
+	const onSubmit = data => {
+		console.log(data)
+	}
+	return (
+		<>
+			<Layout heading="Sign in" bgImage="../../../public/images/auth-bg.png" />
+
+			<div className="wrapper-inner-page">
+				{(isLoading || isLoadingAuth) && <Loader />}
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<Field
+						type="text"
+						name="email"
+						register={register}
+						placeholder="Enter email"
+						error={errors?.email?.message}
+						options={{
+							required: 'Email is required'
+						}}
+					/>
+					<Field
+						type="password"
+						name="password"
+						register={register}
+						placeholder="Enter password"
+						error={errors?.password?.message}
+						options={{
+							required: 'Password is required'
+						}}
+					/>
+					<div className={styles.wrapperButtons}>
+						<Button clickHandler={() => setType('auth')}>Sign in</Button>
+						<Button clickHandler={() => setType('reg')}>Sign up</Button>
+					</div>
+				</form>
+			</div>
+		</>
+	)
 }
